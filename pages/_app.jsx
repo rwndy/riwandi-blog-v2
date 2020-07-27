863a62b63845c2f140bb8afcc5187f390b37e8d4
rwndy <riwandy10@gmail.com>
2020-07-27 17:49:16 +0700

    update new component like  slug, react markdown, etc

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,6 +2,7 @@ import '../public/assets/css/global/index.css'
 import '../public/assets/css/home/home.css'
 import '../public/assets/css/navigation/nav.css'
 import '../public/assets/css/contact/contact.css'
+import '../public/assets/css/about/about.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
---
+++ post/introduction.md
@@ -0,0 +1,5 @@
+---
+author: Riwandi
+date: '2020-07-26'
+title: 'Introduction'
+---
