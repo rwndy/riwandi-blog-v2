fccc4267583757a42e02735fa39d3d6dbe0b3c08
riwandi <riwandy10@gmail.com>
2020-12-10 09:47:13 +0700

    add styling about components

--- pages/about.jsx
+++ pages/about.jsx
@@ -5,7 +5,17 @@ const About = () => {
   return (
     <section>
       <Navbar />
-      <h1>My name is riwandi</h1>
+      <div className="container justify-content-center">
+        <h1 className="text-center text-white mt-4 mb-3">About me</h1>
+        <div className="about-image">
+          <img src="../../assets/images/avatar.jpg" alt="Riwandi's avatar"/>
+        </div>
+        <div className="container">
+          <div className="about-text">
+            <p className=" text-white mt-5">My name is Riwandi. I am a Front-end Developer and also a Content Writer. I made this blog and wrote about daily activities</p>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
