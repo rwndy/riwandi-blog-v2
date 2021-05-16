c497a28a3c1670aaac7df777e27310f48967e720
rwndy <riwandy10@gmail.com>
2021-05-16 14:41:46 +0700

    add licencse pict for image

--- components/Maintance/Maintance.jsx
+++ components/Maintance/Maintance.jsx
@@ -2,9 +2,9 @@
 const Maintance = () => {
   return (
     <div className="container maintance">
-      <div className="maintance__wrapper">
+      <a className="maintance__wrapper" href="https://www.freepik.com/free-vector/website-maintenance-abstract-concept-vector-illustration-website-service-webpage-seo-maintenance-web-design-corporate-site-professional-support-security-analysis-update-abstract-metaphor_11668822.htm#page=1&query=web%20maintenance&position=0" title="Designed by vectorjuice / Freepik">
         <img src="/assets/images/maintance.png" />
-      </div>
+      </a>
       <h1 className="maintance__title">Website sedang dalam perbaikan</h1>
     </div>
   )
