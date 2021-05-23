08f079997c6d8bb9b8359af8d7c6865528010eb3
rwndy <riwandy10@gmail.com>
2021-05-23 13:02:07 +0700

    append portfolio components

--- components/index.js
+++ components/index.js
@@ -3,5 +3,6 @@ import Footer from './Footer/footer'
 import HomeComponent from './Home'
 import Meta from './Meta'
 import Navbar from './Navbar'
+import PortfolioComponent from './Portfolio'
 
-export { AboutMe, Footer, HomeComponent, Meta, Navbar }
+export { AboutMe, Footer, HomeComponent, Meta, Navbar, PortfolioComponent }
