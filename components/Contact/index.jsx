58756f0b2f98c19383c02e959a68c47590c39c1c
rwndy <riwandy10@gmail.com>
2021-05-23 13:06:03 +0700

    append contact

---
+++ components/Contact/index.jsx
@@ -0,0 +1,13 @@
+
+
+const ContactComponent = () => {
+  return (
+    <div className="contact">
+      <div className="container">
+        <h1 className="contact__title">Coming Soon!</h1>
+      </div>
+    </div>
+  )
+}
+
+export default ContactComponent
