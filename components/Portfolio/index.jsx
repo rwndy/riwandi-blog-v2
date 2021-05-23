7dd5283e0b9adcffe5044145e6883323415259db
rwndy <riwandy10@gmail.com>
2021-05-23 13:02:51 +0700

    fix styling

---
+++ components/Portfolio/index.jsx
@@ -0,0 +1,13 @@
+
+
+const PortfolioComponent = () => {
+  return (
+    <div className="portfolio">
+      <div className="container">
+        <h1 className="portfolio__title">Coming Soon!</h1>
+      </div>
+    </div>
+  )
+}
+
+export default PortfolioComponent
