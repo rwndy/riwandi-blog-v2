be38333e1513edfd4674fcb06f42b3aa07341eb1
rwndy <riwandy10@gmail.com>
2021-05-24 11:23:00 +0700

    edit

--- components/Meta/index.jsx
+++ components/Meta/index.jsx
@@ -6,17 +6,17 @@ const Meta = () => {
         <title>Riwandi</title>
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet"></link>
-        <meta name="description" content="Riwandi adalah seorang Penulis dan juga Front-end Developer di Indonesia yang memiliki pengalaman bekerja sebagai front-end developer lebih dari 1 tahun" />
+        <meta name="description" content="Riwandi adalah seorang Penulis dan juga Front-end Developer di Indonesia " />
         <meta name="keywords"
-        content="Riwandi, Penulis, Creative Writing, Front-end Developer" />
+        content="Riwandi, Penulis, Front-end Developer" />
         <meta name="robots" content="index, follow" />
         <meta name="googlebot" content="index, follow" />
         <meta name="distribution" content="global" />
         <meta name="classification" content="Penulis konten, Front-end Developer , Riwandi" />
-        <meta name="mytopic" content="keseharian, keluh-kesah, trend, anak muda" />
+        <meta name="mytopic" content="keseharian, keluh-kesah, anak muda" />
         <meta property="og:title" content="Riwandi" />
         <meta property="og:description"
-        content="Riwandi adalah seorang Penulis dan juga Front-end Developer di Indonesia yang memiliki pengalaman bekerja sebagai front-end developer lebih dari 1 tahun" />
+        content="Riwandi adalah seorang Penulis dan juga Front-end Developer di Indonesia" />
         <meta property="og:image" content="/favicon.ico" />
     </Head>
   )
