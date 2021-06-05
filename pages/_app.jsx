9e7898b1220eab053cbe7250387d8e59cbc35563
rwndy <riwandy10@gmail.com>
2021-06-05 09:37:58 +0700

    append GA

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,12 @@
+import { initGA, logPageView } from '../analytic'
+import Router from 'next/router'
 import '../styles/main.scss'
 
-
-
-
-
-
-
-
-
 export default function MyApp({ Component, pageProps }) {
+  React.useEffect(() => {
+    initGA();
+    logPageView();
+    Router.events.on('routeChangeComplete', logPageView);
+  }, []);
   return <Component {...pageProps} />
 }
